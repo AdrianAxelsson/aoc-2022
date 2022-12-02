6259bcdc7a3ea8ae1b57b7e5f492a11fe86b721b
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-02 10:44:39 +0100

    PEP-8 Formatting https://peps.python.org/pep-0008/

--- 2/main.py
+++ 2/main.py
@@ -10,56 +10,84 @@ scores = {
     'Z': 3
 }
 
+
 def get_rounds():
     rounds = []
     for line in input:
         rounds.append(line.split(' '))
     return rounds
 
+
 def play_round(round):
     opponent, me = round
     score = scores[me]
     if me == 'X':
-        if opponent == 'A': score += 3
-        elif opponent == 'B': score += 0
-        elif opponent == 'C': score += 6
+        if opponent == 'A':
+            score += 3
+        elif opponent == 'B':
+            score += 0
+        elif opponent == 'C':
+            score += 6
     elif me == 'Y':
-        if opponent == 'A': score += 6
-        elif opponent == 'B': score += 3
-        elif opponent == 'C': score += 0
+        if opponent == 'A':
+            score += 6
+        elif opponent == 'B':
+            score += 3
+        elif opponent == 'C':
+            score += 0
     elif me == 'Z':
-        if opponent == 'A': score += 0
-        elif opponent == 'B': score += 6
-        elif opponent == 'C': score += 3
+        if opponent == 'A':
+            score += 0
+        elif opponent == 'B':
+            score += 6
+        elif opponent == 'C':
+            score += 3
     return score
+
 
 def play_round_p2(round):
     opponent, result = round
     if result == 'X':
-        if opponent == 'A': me = 'C'
-        elif opponent == 'B': me = 'A'
-        elif opponent == 'C': me = 'B'
+        if opponent == 'A':
+            me = 'C'
+        elif opponent == 'B':
+            me = 'A'
+        elif opponent == 'C':
+            me = 'B'
     elif result == 'Y':
         me = opponent
     elif result == 'Z':
-        if opponent == 'A': me = 'B'
-        elif opponent == 'B': me = 'C'
-        elif opponent == 'C': me = 'A'
+        if opponent == 'A':
+            me = 'B'
+        elif opponent == 'B':
+            me = 'C'
+        elif opponent == 'C':
+            me = 'A'
 
     score = scores[me]
     if me == 'A':
-        if opponent == 'A': score += 3
-        elif opponent == 'B': score += 0
-        elif opponent == 'C': score += 6
+        if opponent == 'A':
+            score += 3
+        elif opponent == 'B':
+            score += 0
+        elif opponent == 'C':
+            score += 6
     elif me == 'B':
-        if opponent == 'A': score += 6
-        elif opponent == 'B': score += 3
-        elif opponent == 'C': score += 0
+        if opponent == 'A':
+            score += 6
+        elif opponent == 'B':
+            score += 3
+        elif opponent == 'C':
+            score += 0
     elif me == 'C':
-        if opponent == 'A': score += 0
-        elif opponent == 'B': score += 6
-        elif opponent == 'C': score += 3
+        if opponent == 'A':
+            score += 0
+        elif opponent == 'B':
+            score += 6
+        elif opponent == 'C':
+            score += 3
     return score
+
 
 rounds = get_rounds()
 
@@ -70,7 +98,6 @@ for round in rounds:
 p2 = 0
 for round in rounds:
     p2 += play_round_p2(round)
-
 
 print("** AoC 2022 - Day 2 Solution **")
 print(f"Part 1: {p1}")
